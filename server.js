6869e1191fccb5ce25a081ed6196bf11b1271cf6
Shivom Parashari <23bai70407@cuchd.in>
2026-08-07 19:16:25 +0530

    Hii

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const mongoURL = process.env.MONGO_URL
+const PORT = process.env.PORT
+
+
+
+mongoose.connect(mongoURL)
+.then(()=>{console.log("Mongoose connected successfully")})
+.catch(()=>{console.log("Something went wrong")})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{ console.log(`Server is running on ${PORT}`)})
